f325118e84c487271f92d48fdb9dd05ceb893098
Alexey Mikhaylov <alexeysampds@gmail.com>
2025-02-07 15:12:03 +0400

    Updates

---
+++ assets/Logo-c922ad16.js
@@ -0,0 +1,1 @@
+import{j as o,L as s}from"./index-26a57ccc.js";const t="_logo_rhs3t_1",l={logo:t};function r(){return o.jsx(s,{to:"/",children:o.jsx("img",{src:"/logo.png",alt:"Мои путешествия",className:l.logo})})}export{r as L};
